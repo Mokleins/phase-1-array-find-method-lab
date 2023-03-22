34aae2e8b55e40229c78c14113918ed0ca971df1
Xylon Funes <xyfullns@gmail.com>
2023-03-22 00:40:36 -0400

    find function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record) {
+  if(record.find(isWinner) !== undefined){
+    return record.find(isWinner).year;
+  };
+ 
+}
+
+function isWinner(record) {
+  return record.result === "W";
+}
